f8f0be7e533417f6bcd77dbfb105eafdfb3b5f7a
Renad S. Ibrahim <153818206+renadibrahim23@users.noreply.github.com>
2026-09-25 02:16:00 +0300

    Solve relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def relu(x) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    return np.array(np.maximum(0, np.array(x)))
+    
+        
+    
